d3bf22ecf54276cf2dcebd4ffd19a3a7b9aa14d1
Spencer Smith <smithspencer817@gmail.com>
2020-05-07 20:57:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,12 @@
 # Write your code here!
+def game_hash
+  result = Hash.new 
+  result[:home] = {}
+  result[:away] = {}
+  
+  result.each do |key|
+    result[key][:team_name] = ""
+    result[key][:colors] = []
+    result[key][:players] = []
+    
+  end
