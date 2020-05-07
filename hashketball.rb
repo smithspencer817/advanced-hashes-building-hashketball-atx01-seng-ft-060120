a06d094804ed48f4cd935449caf5eaec27da6eea
Spencer Smith <smithspencer817@gmail.com>
2020-05-07 21:21:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,14 +1,5 @@
 # Write your code here!
-require "pry"
 
 def game_hash
-  result = Hash.new 
-  result[:home] = {}
-  result[:away] = {}
-  result.each do |key, value|
-    result[key][:team_name] = ""
-    result[key][:colors] = []
-    result[key][:players] = []
-  end
-  result
+  
 end
